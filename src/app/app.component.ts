95dd01b7fa1143b8b36d3e36229f531a89aa7c4d
Dmitriy Shekhovtsov <valorkin@gmail.com>
2020-11-02 18:35:45 +0200

    feat: input and output value bindings are working properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ import {
 export class LocalInputProviderComponent implements OnChanges {
   @Input() input_prop_1: string;
   @Input() input_prop_2: number;
-  @Output() outout_prop_1 = new EventEmitter<string>();
-  @Output() outout_prop_2 = new EventEmitter<number>();
+  @Output() out_prop_1 = new EventEmitter<string>();
+  @Output() out_prop_2 = new EventEmitter<number>();
 
   ngOnChanges(changes: SimpleChanges): void {
   }
@@ -26,12 +26,12 @@ export class LocalInputProviderComponent implements OnChanges {
   selector: `remote-input-receiver`,
   template: `
     <p>prop 1: {{input_prop_1}}</p>
-    <p><input type="text" (change)="outout_prop_1.emit($event.target.value)"></p>
+    <p><input type="text" (change)="out_prop_1.emit($event.target.value)"></p>
   `
 })
 export class RemoteInputReceiver {
   @Input() input_prop_1: string;
-  @Output() outout_prop_1 = new EventEmitter<string>();
+  @Output() out_prop_1 = new EventEmitter<string>();
 }
 
 @Component({
@@ -43,7 +43,7 @@ export class RemoteInputReceiver {
       <input type="text" (change)="t2 = $event.target.value " [value]="t2">
     </p>
     <p>
-      <local-input-provider #test></local-input-provider>
+      <local-input-provider #test [input_prop_1]="t1" (out_prop_1)="updateT2($event)"></local-input-provider>
       <remote-input-receiver></remote-input-receiver>
     </p>
   `
@@ -54,6 +54,10 @@ export class AppComponent implements AfterViewInit {
   t1 = 'zero input';
   t2 = 'zero no input';
 
+  updateT2(value: any): void {
+    this.t2 = value;
+  }
+
   ngAfterViewInit(): void {
     console.log(this.local);
     console.log(this.remote);
@@ -61,12 +65,22 @@ export class AppComponent implements AfterViewInit {
     // 0. this provides compile time check
     // 1. Add run time checks for namings
     // 2. TBD: how to check input types in runtime?
+    // done - 3. Set initial values
     const inputs = this.local.constructor.ɵcmp.inputs;
     if (inputs) {
       const inputArr = Object.keys(inputs);
+      // set initial values
+      setTimeout(() => {
+        for (const key of inputArr) {
+          this.remote[key] = this.local[key];
+        }
+      });
+
       this.local.ngOnChanges = (value: SimpleChanges): void => {
         if (value) {
-          console.log(value);
+          for (const key of Object.keys(value)) {
+            this.remote[key] = value[key].currentValue;
+          }
         }
       };
     }
@@ -75,7 +89,7 @@ export class AppComponent implements AfterViewInit {
     if (outputs) {
       const outputArr = Object.keys(outputs);
       for (const key of outputArr) {
-        this.remote.ɵcmp.outputs[key].subscribe((value: any) => this.local.ɵcmp)
+        this.remote[key].subscribe((value: any) => this.local[key]?.emit(value));
       }
     }
 

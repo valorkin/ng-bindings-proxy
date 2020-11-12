7e4bbb2310eec4a20fbab0e9bb5b752058f5b242
Dmitriy Shekhovtsov <valorkin@gmail.com>
2020-11-12 15:16:37 +0200

    feat: getComponent for production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,12 +49,91 @@ export class LocalInputProviderComponent implements OnChanges {
 }
 
 @Directive({selector: '[localBindingsAnchor]'})
-export class BindingsProviderAnchor {
+export class BindingsProviderAnchor implements OnInit {
   component: any;
 
-  constructor(private el: ElementRef) {
-    this.component = (window as any).ng.getComponent(el.nativeElement);
+  constructor(private elRef: ElementRef) {
   }
+
+  ngOnInit(): void {
+    this.component = this._getComponent(this.elRef.nativeElement);
+  }
+
+  _getComponent(nativeElement): any {
+
+    const ngDevMode = true;
+    const MONKEY_PATCH_KEY_NAME = '__ngContext__';
+    const TVIEW = 1;
+    const HEADER_OFFSET = 20;
+    const HOST = 0;
+
+    function findViaNativeElement(lView, target) {
+      const tView = lView[TVIEW];
+      for (let i = HEADER_OFFSET; i < tView.bindingStartIndex; i++) {
+        if (unwrapRNode(lView[i]) === target) {
+          return i;
+        }
+      }
+      return -1;
+    }
+
+    /**
+     * Returns `RNode`.
+     * @param value wrapped value of `RNode`, `LView`, `LContainer`
+     */
+    function unwrapRNode(value) {
+      while (Array.isArray(value)) {
+        value = value[HOST];
+      }
+      return value;
+    }
+
+    function readPatchedData(target) {
+      return target[MONKEY_PATCH_KEY_NAME] || null;
+    }
+
+    function getLContext(target) {
+      const mpValue = readPatchedData(target);
+      if (mpValue) {
+        // only when it's an array is it considered an LView instance
+        // ... otherwise it's an already constructed LContext instance
+        if (Array.isArray(mpValue)) {
+          const lView = mpValue;
+          let nodeIndex;
+          nodeIndex = findViaNativeElement(lView, target);
+          if (nodeIndex === -1) {
+            return null;
+          }
+          return {nodeIndex, lView};
+        }
+      }
+    }
+
+    function getComponentAtNodeIndex(nodeIndex, lView) {
+      const tNode = lView[TVIEW].data[nodeIndex];
+      let directiveStartIndex = tNode.directiveStart;
+      return tNode.flags & 2 /* isComponentHost */ ? lView[directiveStartIndex] : null;
+    }
+
+    function loadLContext(target, throwOnNotFound = true) {
+      const context = getLContext(target);
+      return context;
+    }
+
+    function getComponent(element) {
+      const context = loadLContext(element, false) as any;
+      if (context === null) {
+        return null;
+      }
+      if (context.component === undefined) {
+        context.component = getComponentAtNodeIndex(context.nodeIndex, context.lView);
+      }
+      return context.component;
+    }
+
+    return getComponent(nativeElement);
+  }
+
 }
 
 /**
